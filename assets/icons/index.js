63ec77ee5281d33f16b0b997a094c3c32df753a3
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2020-04-15 20:04:31 +0530

    chore: removed unused icons

--- assets/icons/index.js
+++ assets/icons/index.js
@@ -1,7 +1,4 @@
-import Menu from './Menu'
 import Home from './Home'
 import Redirect from './Redirect'
-import Tv from './Tv'
-import Code from './Code'
 
-export { Menu, Redirect, Home, Tv, Code }
+export { Redirect, Home }
