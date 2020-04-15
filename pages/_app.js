f0cd861e38f0c3e86e8a737c048830d43451d881
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2020-04-15 20:41:27 +0530

    chore: meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
+import Head from 'next/head'
+
 import '../global.css'
 
-export default function MyApp({ Component, pageProps }) {
-   return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps, children }) {
+   return (
+      <Component {...pageProps}>
+         <Head>
+            <title>Sanjeev Sharma</title>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta
+               name="description"
+               content="Full stack developer based in Bengaluru, Karnataka(IN)"
+            />
+            <meta name="robots" content="index, follow" />
+         </Head>
+         {children}
+      </Component>
+   )
 }
