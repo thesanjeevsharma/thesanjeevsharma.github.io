303f8779d3ac2385b4d85a5877bb8ccd19d3e49f
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2020-04-15 19:50:38 +0530

    feat: courses

---
+++ pages/courses.js
@@ -0,0 +1,16 @@
+import { Layout } from '../components'
+
+const Courses = () => {
+   return (
+      <Layout>
+         <React.Fragment>
+            <section className="min-h-screen py-16 text-white text-center py-16">
+               <h1 className="text-4xl font-bold text-green-400 mb-8">Courses</h1>
+               <p className="mb-8">Will be out soon!</p>
+            </section>
+         </React.Fragment>
+      </Layout>
+   )
+}
+
+export default Courses
