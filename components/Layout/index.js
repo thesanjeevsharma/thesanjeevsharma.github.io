fbd72c9e0b1a081439a44f81398cea134b0610c1
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2020-04-15 22:32:09 +0530

    feat: added deploy script

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -7,14 +7,13 @@ const Layout = ({ children }) => {
       <React.Fragment>
          <Head>
             <title>Sanjeev Sharma</title>
-            <meta charset="UTF-8" />
+            <meta charSet="UTF-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <link rel="icon" href="/favicon.ico" />
             <meta
                name="description"
                content="Full stack developer based in Bengaluru, Karnataka(IN)"
             />
-            <meta name="robots" content="index, follow" />
          </Head>
          <Navbar />
          <main className="p-4 lg:px-16 pt-16 bg-matte">{children}</main>
