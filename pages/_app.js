bc297a89dc393dd38a91dc7627014f591dd2a9f2
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2020-04-15 22:08:24 +0530

    fix: Head

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,5 @@
-import Head from 'next/head'
-
 import '../global.css'
 
-export default function MyApp({ Component, pageProps, children }) {
-   return (
-      <Component {...pageProps}>
-         <Head>
-            <title>Sanjeev Sharma</title>
-            <meta charset="UTF-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <link rel="icon" href="/favicon.ico" />
-            <meta
-               name="description"
-               content="Full stack developer based in Bengaluru, Karnataka(IN)"
-            />
-            <meta name="robots" content="index, follow" />
-         </Head>
-         {children}
-      </Component>
-   )
+export default function MyApp({ Component, pageProps }) {
+   return <Component {...pageProps} />
 }
