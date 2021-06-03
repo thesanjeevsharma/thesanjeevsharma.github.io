ef2d8e7533864bf0f2f5809b37032a374f78a9a3
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2021-06-03 13:46:20 +0530

    feat: add og meta tags

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -5,16 +5,6 @@ import { Navbar, Footer } from '../'
 const Layout = ({ children }) => {
    return (
       <React.Fragment>
-         <Head>
-            <title>Sanjeev Sharma</title>
-            <meta charSet="UTF-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <link rel="icon" href="/favicon.ico" />
-            <meta
-               name="description"
-               content="Full stack developer based in Bengaluru, Karnataka(IN)"
-            />
-         </Head>
          <Navbar />
          <main className="p-4 lg:px-16 pt-16 bg-matte">{children}</main>
          <Footer />
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
+import Head from 'next/head'
 import '../global.css'
 
 export default function MyApp({ Component, pageProps }) {
-   return <Component {...pageProps} />
+   return (
+      <>
+         <Head>
+            <title>Sanjeev Sharma | JavaScript Developer</title>
+            <meta charSet="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <link rel="icon" href="/favicon.ico" />
+            <meta property="og:title" content="Sanjeev Sharma" />
+            <meta property="og:description" content="A JavaScript developer based in India" />
+            <meta property="tw:title" content="Sanjeev Sharma" />
+            <meta property="tw:description" content="A JavaScript developer based in India" />
+         </Head>
+         <Component {...pageProps} />
+      </>
+   )
 }
